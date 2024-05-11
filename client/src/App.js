0d4d79293f1e265276df92d6b4574feded79ccb7
maexpe <matheusexpedito58@gmail.com>
2024-05-10 23:29:24 -0300

    feat: Adiciona parâmetros ao componente App para administrar a estrutura UI, incluindo abas para listas diferentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TaskList from './components/TaskList';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/tasks')
+      .then(res => res.json())
+      .then(data => setTasks(data));
+  }, []);
+
+  const handleTaskAction = (action, taskId) => {
+    // TODO
+    // Update task state based on action
+    // Send API request to update backend
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>ReacTask</h1>
+      <TaskList tasks={tasks} onTaskAction={handleTaskAction} />
     </div>
   );
-}
+};
 
 export default App;
